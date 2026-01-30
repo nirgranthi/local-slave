19f56e0bdcb79e8a4513e13ece343c055e4d8db7
Nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2026-01-30 10:09:43 +0530

    fixed input box and send button inputvalue

--- local-slave/src/components/InputArea.jsx
+++ local-slave/src/components/InputArea.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import { SendButton } from "./buttons/SendButton";
 
 export function InputArea() {
-    return (
-        <div className="absolute bottom-0 w-full bg-gray-900 pt-4 pb-4 px-4 border-t border-gray-800">
-          <div className="max-w-3xl mx-auto relative flex gap-2">
-            <input id="user-input" type="text"
-              className="flex-1 bg-gray-800 text-white rounded-lg px-4 py-3 border border-gray-700 focus:outline-none focus:border-blue-500 transition-all"
-              placeholder="Load a model to start..." />
-            <SendButton />
-          </div>
-        </div>
-    )
+  const [inputValue, setInputValue] = useState('')
+  const sendMessage = (value) => { console.log(value) };
+  return (
+    <div className="absolute bottom-0 w-full bg-gray-900 pt-4 pb-4 px-4 border-t border-gray-800">
+      <div className="max-w-3xl mx-auto relative flex gap-2">
+        <input
+          id="user-input"
+          type="text"
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={(e) => { if (e.key == 'Enter') (sendMessage(inputValue)) }}
+          className="flex-1 bg-gray-800 text-white rounded-lg px-4 py-3 border border-gray-700 focus:outline-none focus:border-blue-500 transition-all"
+          placeholder="Load a model to start..." />
+        <SendButton inputValue={inputValue} sendMessage={sendMessage} />
+      </div>
+    </div>
+  )
 }
--- local-slave/src/components/buttons/SendButton.jsx
+++ local-slave/src/components/buttons/SendButton.jsx
@@ -1,7 +1,9 @@
-export function SendButton() {
-    const sendMessage = () => {console.log('message sent')};
+export function SendButton({ inputValue, sendMessage }) {
+
     return (
-        <button id="send-btn" onClick={sendMessage}
+        <button
+            id="send-btn"
+            onClick={sendMessage(inputValue)}
             className="bg-blue-600 px-6 rounded-lg text-white font-medium disabled:opacity-50 disabled:cursor-not-allowed hover:bg-blue-700 transition-colors"
         >
             Send
