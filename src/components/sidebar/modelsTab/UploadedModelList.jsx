59db88cff24946d52817393ceed558500d1b7878
Nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2026-02-03 10:52:29 +0530

    added option to switch between models pasted through links

--- src/components/sidebar/modelsTab/UploadedModelList.jsx
+++ src/components/sidebar/modelsTab/UploadedModelList.jsx
@@ -2,7 +2,20 @@ import { useState } from "react"
 
 export function UploadedModelList({ setSelectedModelUrl }) {
     const [userModelUrl, setUserModelUrl] = useState('')
-    const sendMessage = () => { setSelectedModelUrl(userModelUrl), setUserModelUrl('') }
+    const [UploadedModelUrls, setUploadedModelUrls] = useState([])
+
+    const sendMessage = () => {
+        if (!userModelUrl.trim()) return;
+
+        setSelectedModelUrl(userModelUrl)
+
+        if (!UploadedModelUrls.includes(userModelUrl)) {
+            setUploadedModelUrls(prev => [...prev, userModelUrl])
+        }
+
+        setUserModelUrl('')
+    }
+
     return (
         <div className="w-full text-left p-2.5 rounded-lg border transition-all group relative mb-1 bg-green-900/20 border-green-800 hover:bg-green-900/40">
             <input
@@ -11,7 +24,7 @@ export function UploadedModelList({ setSelectedModelUrl }) {
                 placeholder="Paste GGUF model URL here..."
                 onChange={(e) => setUserModelUrl(e.target.value)}
                 value={userModelUrl}
-                onKeyDown={(e) => {if (e.key === 'Enter') sendMessage()}}
+                onKeyDown={(e) => { if (e.key === 'Enter') sendMessage() }}
             />
 
             <div className="flex justify-between items-center mt-1 pointer-events-none">
@@ -21,6 +34,19 @@ export function UploadedModelList({ setSelectedModelUrl }) {
                     </span>
                 </div>
             </div>
+
+            {UploadedModelUrls.map((url, index) => (
+                <button key={index}
+                    className='w-full text-left p-2.5 rounded-lg border transition-all group relative mb-1 bg-green-900/20 border-green-800 hover:bg-green-900/40'
+                    onClick={() => (setSelectedModelUrl(url))}
+                >
+                    <div className="flex justify-between items-center">
+                        <div className="flex flex-col min-w-0 flex-1">
+                            <span className="font-medium text-xs text-gray-300 group-hover:text-white truncate">{url.split('/').pop()}</span>
+                        </div>
+                    </div>
+                </button>
+            ))}
 
         </div>
     )
