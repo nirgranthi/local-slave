18fc95d9e90f3e250fd95042a74dc6df35a3be6f
Nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2026-02-09 20:21:56 +0530

    input box now changes its height beautifully

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { SendButton } from "./buttons/SendButton.jsx";
 
 export function InputArea({ setUserPrompt, modelStatus, isLiveTokenLive, stopModelReplyRef }) {
   const [inputValue, setInputValue] = useState('')
+  const textAreaRef = useRef(null)
+
+  useEffect(() => {
+    if (textAreaRef) {
+      textAreaRef.current.style.height = 'auto'
+      textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`
+    }
+  },[inputValue])
 
   const handleKeyDown = (e) => {
     if (e.key === 'Enter' && !e.shiftKey) {
@@ -26,14 +34,16 @@ export function InputArea({ setUserPrompt, modelStatus, isLiveTokenLive, stopMod
     <div className="w-full bg-gray-900 pt-4 pb-4 px-4 border-t border-gray-800">
       <div className="max-w-3xl mx-auto relative flex gap-2">
         <textarea
-          id="user-input"
+          ref={textAreaRef}
           type="text"
           onChange={(e) => setInputValue(e.target.value)}
           value={inputValue}
           onKeyDown={handleKeyDown}
           disabled={modelStatus === 'OFFLINE'}
-          className="flex-1 bg-gray-800 text-white rounded-lg px-4 py-3 border border-gray-700 focus:outline-none focus:border-blue-500 transition-all"
-          placeholder="Load a model to start..." />
+          className="flex-1 bg-gray-800 text-white rounded-lg px-4 py-3 border border-gray-700 focus:outline-none focus:border-blue-500 transition-all resize-none max-h-60 overflow-y-auto"
+          placeholder="Load a model to start..."
+          style={{minHeight : '44px'}}
+          />
 
         <SendButton
           inputValue={inputValue}
