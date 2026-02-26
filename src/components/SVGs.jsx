bce1e996e1fe9d6b8b94f5001aad6427a5f138be
Nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2026-02-26 16:33:07 +0530

    added hints to the prompt configs

--- src/components/SVGs.jsx
+++ src/components/SVGs.jsx
@@ -64,3 +64,22 @@ export function UploadSvg() {
         </svg>
     )
 }
+
+export function InfoSvg() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <circle cx="12" cy="12" r="10" />
+            <path d="M12 16v-4" /><path d="M12 8h.01" />
+        </svg>
+    )
+}
--- src/components/sidebar/PromptConfigTab.jsx
+++ src/components/sidebar/PromptConfigTab.jsx
@@ -1,18 +1,23 @@
-
-
-export function PromptConfigTab({selectedTab, promptConfigControlPanel, setPromptConfig}) {
+export function PromptConfigTab({ selectedTab, promptConfigControlPanel, setPromptConfig }) {
     const handleInputChangeForPrompt = (key, value) => {
         setPromptConfig(prev => ({
             ...prev,
             [key]: value
         }))
     }
+    console.log(promptConfigControlPanel)
     return (
         <div className={`h-64 overflow-y-auto pr-2 space-y-3 custom-scrollbar ${selectedTab === 'promptConfigTab' ? '' : 'hidden'}`}>
             {promptConfigControlPanel.map((values) => (
-                <div key={values.id} className="flex flex-col space-y-1">
-                    <div className="flex justify-between text-xs text-gray-400">
-                        <span> {values.label} </span>
+                <div key={values.id} className="flex flex-col space-y-1 pt-5">
+                    <div className="py-2 flex justify-between text-xs text-gray-400">
+                        <div className="flex items-center space-x-1 relative group">
+                            <span>{values.label}</span>
+
+                            <div className="-translate-x-1/2 absolute left-full top-1/2 -translate-y-1/2 ml-15 hidden group-hover:block w-48 p-2 bg-gray-900 text-gray-100 text-[10px] leading-relaxed rounded-md shadow-2xl border border-gray-700 pointer-events-none animate-in fade-in slide-in-from-left-1 duration-200">
+                                {values.hint || "No hint provided, use your brain."}
+                            </div>
+                        </div>
                         <span> {values.value} </span>
                     </div>
                     <input
