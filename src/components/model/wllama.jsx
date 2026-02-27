4be6c5c7a1de3132f2482ac96da66f88c37065d9
Nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2026-02-27 11:14:40 +0530

    disabled eslint dep error

--- src/components/model/wllama.jsx
+++ src/components/model/wllama.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect, useRef } from 'react';
 import { Wllama } from '@wllama/wllama';
 import { friendlyPrompt } from './systemPrompt.jsx';
