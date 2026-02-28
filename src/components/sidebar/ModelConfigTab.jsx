0ee8b4f597c26e143f80f97b63c49c725b9fb67e
Nirgranthi <223480893+nirgranthi@users.noreply.github.com>
2026-02-28 13:40:06 +0530

    updated hint and config tabs UI

--- src/components/sidebar/ModelConfigTab.jsx
+++ src/components/sidebar/ModelConfigTab.jsx
@@ -9,12 +9,12 @@ export function ModelConfigTab({ selectedTab, modelConfig, setModelConfig }) {
     }
     const modelConfigControlPanel = modelConfigCPF(modelConfig)
     return (
-        <div className={`h-64 overflow-y-auto pr-2 space-y-3 custom-scrollbar ${selectedTab === 'modelConfigTab' ? '' : 'hidden'}`}>
+        <div className={`[&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] bg-gray-900 border-t border-gray-700 h-64 overflow-y-auto p-4 space-y-3 custom-scrollbar ${selectedTab === 'modelConfigTab' ? '' : 'hidden'}`}>
             {modelConfigControlPanel.map((values) => (
                 <div key={values.id} className="flex flex-col space-y-1">
-                    <div className="flex py-2 justify-between text-xs text-gray-400 group">
+                    <div className="flex py-4 justify-between text-xs text-gray-400 group">
                         <span> {values.label} </span>
-                        <div className="-translate-x-1/2 absolute top-1/2 -translate-y-1/2 ml-15 hidden group-hover:block w-48 p-2 bg-gray-900 text-gray-100 text-[10px] leading-relaxed rounded-md shadow-2xl border border-gray-700 pointer-events-none animate-in fade-in slide-in-from-left-1 duration-200">
+                        <div className="-translate-x-1/2 absolute top-1/2 -translate-y-1/2 ml-45 hidden group-hover:block w-48 p-2 bg-gray-900 text-gray-100 text-[10px] leading-relaxed rounded-md shadow-2xl border border-gray-700 pointer-events-none animate-in fade-in slide-in-from-left-1 duration-200">
                             {values.hint || "No hint provided, use your brain."}
                         </div>
                         <span> {values.value} </span>
@@ -25,7 +25,7 @@ export function ModelConfigTab({ selectedTab, modelConfig, setModelConfig }) {
                             type={values.type}
                             min={values.min}
                             max={values.max}
-                            step='1'
+                            step={values.step}
                             value={values.value}
                             onChange={(e) => handleInputChangeForModel(values.id, e.target.value)}
                             className="w-full h-1 bg-gray-700 rounded-lg appearance-none cursor-pointer accent-blue-500"
